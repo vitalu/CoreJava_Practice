77e243772556d931f0216724920898aba6773719
Vital Kumar U <vital.kumar77@gmail.com>
2022-03-18 10:07:34 +0530

    First commit

---
+++ CoreJava_Practice/src/DataTypes/Datatypes_Practice.java
@@ -0,0 +1,43 @@
+package DataTypes;
+/*
+Notes: Data types specify the different size and values that can be stored in a variable
+Type of data types: Primitive data types, Non-Primitive data types
+Primitive data type: int, double, float, char, boolean
+Non-Primitive data type: Includes Classes(String), Arrays, Interfaces
+*/
+public class Datatypes_Practice {
+
+	//main() method is starting/execution point of the program
+	public static void main(String[] args) {
+		//1. data type : int
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		//2. data type : double
+		double d = 1.2;
+		double e = 0.33;
+		double f = 1; //can store integer value in double 
+		double g = -1.2;
+		
+		//3. data type : char
+		char h = 'a';
+		char i = '9';//char should be allow only single digit value
+		
+		//4. data type : boolean
+		boolean k = true;
+		boolean l = false;
+		
+		//5. data type : String - this is not data type,it's a class
+		String m = "Data types";
+		String n = "223";
+		String o = "22.222";
+		String p = "p";
+		
+		
+		
+		
+
+	}
+
+}
